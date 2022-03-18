e08460288113954af435a0ddb61d620598795838
Alexander Choban <alexander.choban@gmail.com>
2022-03-18 15:41:01 -0500

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8e34022af6717465854a";
+/******/ 	var hotCurrentHash = "527ec2b96fb18346c165";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
